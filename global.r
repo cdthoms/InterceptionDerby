dcb06caf0ae5772511e7c34e7ca50e9ad40fd915
cthoms <cthoms@uber.com>
2014-11-16 10:46:27 -0800

    Fixed QB list to yahoo and not broken ESPN.

--- global.r
+++ global.r
@@ -18,15 +18,19 @@ qb_int_list = readHTMLTable(qb_int_url,stringsAsFactors=F)
 team_int_url = 'http://www.sportingcharts.com/nfl/stats/team-interception-rate/2014/'
 team_int_list = readHTMLTable(team_int_url,stringsAsFactors=F)
 
-qb_url = 'http://espn.go.com/nfl/players/_/position/qb'
+# qb_url = 'http://espn.go.com/nfl/players/_/position/qb'
+# qb_list = readHTMLTable(qb_url,stringsAsFactors=F)
+# qbframe = qb_list[[1]]
+# qbframe = qbframe[,c(1:2)]
+# names(qbframe) = c('Name','Team')
+# qbframe = qbframe[grep(',',qbframe$Name),]
+
+qb_url = 'http://sports.yahoo.com/nfl/players?type=position&c=NFL&pos=QB'
 qb_list = readHTMLTable(qb_url,stringsAsFactors=F)
-
-qbframe = qb_list[[1]]
-qbframe = qbframe[,c(1:2)]
-
-names(qbframe) = c('Name','Team')
-
-qbframe = qbframe[grep(',',qbframe$Name),]
+qbs = qb_list[[9]]
+qbs = qbs[-1,]
+firstlast = colsplit(qbs[,1]," ",c("first","last"))
+qbframe = data.frame(Name = paste0(firstlast$last,', ',firstlast$first),Team = qbs[,3])
 
 sched = read.csv('nfl2014sched.csv',stringsAsFactors=F)
 sched$DATE = as.Date(sched$DATE,format='%m/%d/%y')
@@ -34,8 +38,6 @@ sched$DATE = as.Date(sched$DATE,format='%m/%d/%y')
 weeklyDate = aggregate(sched$DATE,list(week=sched$WEEK),max)
 
 currentWeek = min(which(weeklyDate$x>=Sys.Date()))
-
-# info = read.csv('2014derbypicks.csv',stringsAsFactors=F)
 
 myCsv = getURL('https://docs.google.com/spreadsheet/pub?key=0Akv8ehIfGJVKdGx3NXdib0p6Q01CSEZ4amY0RW53Ymc&single=true&gid=2&output=csv',
                ssl.verifypeer=F,useragent='R')
